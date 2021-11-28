b9cc146bc8a12c4501e41fd15377ed586bda20fc
Iqbal Alfikri <iqbalalfikri12@gmail.com>
2021-11-28 21:34:50 +0700

    Add and Get User implemented

--- migrations/1638105798646_create-table-users.js
+++ migrations/1638105798646_create-table-users.js
@@ -1,27 +1,21 @@
 /* eslint-disable camelcase */
+
 exports.up = (pgm) => {
-  pgm.createTable('notes', {
+  pgm.createTable('users', {
     id: {
       type: 'VARCHAR(50)',
       primaryKey: true,
     },
-    title: {
+    username: {
+      type: 'VARCHAR(50)',
+      unique: true,
+      notNull: true,
+    },
+    password: {
       type: 'TEXT',
       notNull: true,
     },
-    body: {
-      type: 'TEXT',
-      notNull: true,
-    },
-    tags: {
-      type: 'TEXT[]',
-      notNull: true,
-    },
-    created_at: {
-      type: 'TEXT',
-      notNull: true,
-    },
-    updated_at: {
+    fullname: {
       type: 'TEXT',
       notNull: true,
     },
@@ -29,5 +23,5 @@ exports.up = (pgm) => {
 };
 
 exports.down = (pgm) => {
-  pgm.dropTable('notes');
+  pgm.dropTable('users');
 };
